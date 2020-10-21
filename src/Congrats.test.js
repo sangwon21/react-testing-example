2fbb7ade3d968a8ab01052c122dde90c181273ee
sangwon <junothyme@gmail.com>
2020-10-21 23:37:53 +0900

    feat: Added proptypes for Congrats component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import checkPropTypes from "check-prop-types";
 
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-const setUp = (props = {}) => shallow(<Congrats {...props} />);
+const defaultProps = { success: false };
+
+const setUp = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
+};
 
 test("renders without error", () => {
-  const wrapper = setUp();
+  const wrapper = setUp({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
 
   expect(component.length).toBe(1);
@@ -29,4 +35,9 @@ test("renders non-empty congrats message when success prop is true", () => {
   const message = findByTestAttr(wrapper, "congrats-message");
 
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
